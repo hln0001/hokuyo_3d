bbbbc993df7e2c71d152bab882c96d3ca83813ca
Hayden Nichols <hln0001@mix.wvu.edu>
2018-06-12 14:28:01 -0400

    changes to revolution snapshotter

--- src/revolution_snapshotter.cpp
+++ src/revolution_snapshotter.cpp
@@ -11,8 +11,8 @@ int rotNum;
 
 void rotNumCallback(std_msgs::UInt16 rotation_number)
 {
-  rotNum = rotation_number.data;
-}
+  rotNum = rotation_number.data;  
+}  
 
 namespace laser_assembler
 {
@@ -21,28 +21,28 @@ class PeriodicSnapshotter
 {
 
 public:
+  ros::NodeHandle n_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
 
   PeriodicSnapshotter()
   {
     // Create a publisher for the clouds that we assemble
     pub_ = n_.advertise<sensor_msgs::PointCloud2> ("velodyne_points", 1); //must be "velodyne_points" for blam_slam node
+    sub_ = n_.subscribe<std_msgs::UInt16>("rotation_count", 1, rotNumCallback);
 
-    sub_ = n_.subscribe<std_msgs::UInt16>("rotation_number", 1, rotNumCallback);
+    serviceTimer();
 
     // Create the service client for calling the assembler
     client_ = n_.serviceClient<AssembleScans2>("assemble_scans2");
 
     last_time = ros::Time(0,0);
 
-    // Start the timer that will trigger the processing loop (timerCallback)
-    PeriodicSnapshotter::timerCallback();
-
     pubNum = 0;
   }
 
-  void timerCallback()
+  void serviceTimer()
   {
-
     if(rotNum != pubNum)
     {
       // Populate our service request based on our timer callback times
@@ -53,23 +53,14 @@ public:
       // Make the service call
       if (client_.call(srv))
       {
-        // ROS_INFO("Published Cloud with %u points", (uint32_t)(srv.response.cloud.points.size())) ;
         pub_.publish(srv.response.cloud);
         pubNum++;
         last_time = ros::Time::now();
-
-      }
-      else
-      {
-        // ROS_ERROR("Error making service call\n") ;
-      }
-    }
-  }
+       }
+     }
+   }
 
 private:
-  ros::NodeHandle n_;
-  ros::Publisher pub_;
-  ros::Subscriber sub_;
   ros::ServiceClient client_;
   ros::Time last_time;
   ros::Timer timer_;
