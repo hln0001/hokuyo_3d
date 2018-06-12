5a98e35ac379e7b1d63eee49aa4568c0fd6808d5
Hayden Nichols <hln0001@mix.wvu.edu>
2018-06-12 15:34:33 -0400

    add periodic snapshot launch file for testing

--- src/periodic_snapshotter.cpp
+++ src/periodic_snapshotter.cpp
@@ -24,7 +24,7 @@ public:
     client_ = n_.serviceClient<AssembleScans2>("assemble_scans2");
 
     // Start the timer that will trigger the processing loop (timerCallback)
-    timer_ = n_.createTimer(ros::Duration(2.5,0), &PeriodicSnapshotter::timerCallback, this); //change this line to change frequency
+    timer_ = n_.createTimer(ros::Duration(1,0), &PeriodicSnapshotter::timerCallback, this); //change this line to change frequency
 //                                        ^this value
     // Need to track if we've called the timerCallback at least once
     first_time_ = true;
